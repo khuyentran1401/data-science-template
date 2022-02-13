73bd6dda2d404e3e2ba7dcad46c6b7e70be8ce74
khuyentran1401 <khuyentran1476@gmail.com>
2022-02-12 19:35:51 -0600

    edit README

--- src/predict.py
+++ src/predict.py
@@ -3,7 +3,7 @@ import requests
 prediction = requests.post(
     "http://127.0.0.1:5000/predict",
     headers={"content-type": "application/json"},
-    data='{"Income": 58138, "Recency": 58,"MntWines": 635,"MntFruits": 88,"MntMeatProducts": 546,"MntFishProducts": 172,"MntSweetProducts": 88,"MntGoldProds": 88,"Complain": 0,"Response": 1,"age": 64,"enrollment_years": 10,"family_size": 1}',
+    data='{"Income": 58138, "Recency": 58, "Complain": 0,"age": 64,"total_purchases": 25,"enrollment_years": 10,"family_size": 1}',
 ).text
 
 print(prediction)
