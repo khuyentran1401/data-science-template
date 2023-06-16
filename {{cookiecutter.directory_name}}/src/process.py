fb9f9855ab64f116ac4f911dfd0cc14d6ae8313d
khuyentran1401 <khuyentran1476@gmail.com>
2023-06-16 14:06:28 -0500

    Refactor config parameter in process_data and train_model functions

--- {{cookiecutter.directory_name}}/src/process.py
+++ {{cookiecutter.directory_name}}/src/process.py
@@ -5,10 +5,11 @@ Author: Khuyen Tran
 """
 
 import hydra
-from omegaconf import DictConfig
 from hydra.utils import to_absolute_path as abspath
+from omegaconf import DictConfig
 
-@hydra.main(config_path="../config", config_name='main')
+
+@hydra.main(config_path="../config", config_name="main", version_base=None)
 def process_data(config: DictConfig):
     """Function to process the data"""
 
@@ -16,5 +17,6 @@ def process_data(config: DictConfig):
     print(f"Process data using {raw_path}")
     print(f"Columns used: {config.process.use_columns}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     process_data()
--- {{cookiecutter.directory_name}}/src/train_model.py
+++ {{cookiecutter.directory_name}}/src/train_model.py
@@ -1,15 +1,15 @@
 """
 This is the demo code that uses hy                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      dra to access the parameters in under the directory config.
 
-Author: Khuyen Tran 
+Author: Khuyen Tran
 """
 
 import hydra
-from omegaconf import DictConfig
 from hydra.utils import to_absolute_path as abspath
+from omegaconf import DictConfig
 
 
-@hydra.main(config_path="../config", config_name="main")
+@hydra.main(config_path="../config", config_name="main", version_base=None)
 def train_model(config: DictConfig):
     """Function to train the model"""
 
