2efe7cc9271589bd4553522a8101a3c706b62a31
Pranav Arora <pranava880@gmail.com>
2023-08-03 16:08:28 -0400

    Added basic logging
    Configured logging procedures and log file.

--- {{cookiecutter.directory_name}}/src/process.py
+++ {{cookiecutter.directory_name}}/src/process.py
@@ -6,8 +6,12 @@ Author: Pranav Arora
 
 import hydra
 import pandas as pd
+import logging
 from omegaconf import DictConfig
 
+# configure logging
+FORMAT = '%(asctime)s || %(levelname)s - %(message)s'
+logging.basicConfig(format=FORMAT, datefmt='%d/%m/%Y %H:%M:%S', filename='process.log', level=logging.DEBUG)
 
 @hydra.main(config_path="../config", config_name="main", version_base=None)
 def process_data(config: DictConfig):
@@ -16,8 +20,11 @@ def process_data(config: DictConfig):
     print(f"Process data using {config.data.raw}")
     print(f"Columns used: {config.process.use_columns}")
 
+    logging.debug('Parsing csv...')
     data = pd.read_csv(config.data.raw)
+
     print(data.to_string())
+    logging.debug(data.to_string())
 
 
 
