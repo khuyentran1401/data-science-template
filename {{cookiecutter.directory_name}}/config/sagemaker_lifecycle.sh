ff2298ef407fbe0d903929a06c19997dfcaff330
Rubem Pacelli <rubem.engenharia@gmail.com>
2024-01-25 10:47:11 -0300

    Update sagemaker_lifecycle.sh

--- {{cookiecutter.directory_name}}/config/sagemaker_lifecycle.sh
+++ {{cookiecutter.directory_name}}/config/sagemaker_lifecycle.sh
@@ -9,12 +9,15 @@ git config --global user.name "{{ cookiecutter.your_github_repo.split('/')[3] }}
 git config --global credential.helper cache
 git config --global credential.helper "cache --timeout=86400"
 # remove conda environment
-sudo rm -rf /opt/conda ~/.conda
+sudo rm -rf /opt/conda
+rm -rf ~/.conda
 # install vanilla python interpreter
+sudo apt update
 sudo apt install -y python3 python3-pip
 # install poetry
 pip install poetry
 # add user ~/.local/bin to $PATH
-sudo sed -i '$a export PATH="~/.local/bin${PATH:+:${PATH}}"' /etc/bash.bashrc
+sudo sed -i '$a export PATH="/home/sagemaker-user/.local/bin${PATH:+:${PATH}}"' /etc/bash.bashrc
+source /etc/bash.bashrc
 # clone the repo
 git clone -C /home/sagemaker-user clone {{ cookiecutter.your_github_repo }}
