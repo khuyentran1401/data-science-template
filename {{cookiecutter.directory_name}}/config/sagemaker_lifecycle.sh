2d687465570570697e7118ab64fe9da939512825
Rubem Pacelli <rubem.engenharia@gmail.com>
2024-01-26 07:20:03 -0300

    Update sagemaker_lifecycle.sh

--- {{cookiecutter.directory_name}}/config/sagemaker_lifecycle.sh
+++ {{cookiecutter.directory_name}}/config/sagemaker_lifecycle.sh
@@ -14,6 +14,6 @@ sudo apt install -y python3
 # install poetry
 pip install poetry
 # add user ~/.local/bin to $PATH
-echo '$a export PATH="/home/sagemaker-user/.local/bin${PATH:+:${PATH}}"' >> /home/sagemaker-user/.bashrc
+echo 'export PATH="/home/sagemaker-user/.local/bin${PATH:+:${PATH}}"' >> /home/sagemaker-user/.bashrc
 # clone the repo
 git -C /home/sagemaker-user clone {{ cookiecutter.your_github_repo }}
