934a7c2d6cbd2ea6db9b9a98492ab6c9b7da74d0
khuyentran1401 <khuyentran1476@gmail.com>
2024-02-04 16:19:18 -0600

    delete jinja extension and fix post_gen_project.py

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,8 @@
+import os
+
+def parse_dependencies_settings():
+    if "{{ cookiecutter.dependency_manager }}" == "pip":
+        os.remove("requirements.txt")
+
+if __name__ == "__main__":
+    parse_dependencies_settings()
